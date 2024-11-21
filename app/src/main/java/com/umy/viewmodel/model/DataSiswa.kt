d8773d38ad1a2a0b84c3395c679217989256da1d
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-11-21 20:17:52 +0700

    Penambahan variabel Nim didalam model DataSiswa

--- app/src/main/java/com/umy/viewmodel/model/DataSiswa.kt
+++ app/src/main/java/com/umy/viewmodel/model/DataSiswa.kt
@@ -2,6 +2,7 @@ package com.umy.viewmodel.model
 
 data class DataSiswa(
     val nama: String = "",
+    val nim: String = "",
     val alamat: String = "",
     val gender: String = "",
     val notelpon: String = ""
